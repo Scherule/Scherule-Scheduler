ddfe0213d3ca163559808d18262e22ee8c395444
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-06-27 20:22:19 +0200

    Example json response

--- src/main/kotlin/com/scherule/scheduling/SchedulingRootVerticle.kt
+++ src/main/kotlin/com/scherule/scheduling/SchedulingRootVerticle.kt
@@ -1,40 +1,27 @@
 package com.scherule.scheduling;
 
 import com.scherule.commons.MicroServiceVerticle
-import io.vertx.core.json.JsonObject
-import io.vertx.servicediscovery.ServiceDiscovery
-import io.vertx.servicediscovery.ServiceDiscoveryOptions
+import io.vertx.rx.java.RxHelper
+import io.vertx.rxjava.core.eventbus.MessageConsumer
+import org.slf4j.LoggerFactory
 
 class SchedulingRootVerticle : MicroServiceVerticle() {
 
+    companion object {
+        val log = LoggerFactory.getLogger(SchedulingRootVerticle::class.qualifiedName)
+    }
+
+    private lateinit var messageConsumer: MessageConsumer<String>
+
     override fun start() {
-        val discovery = ServiceDiscovery.create(vertx,
-                ServiceDiscoveryOptions()
-                        .setAnnounceAddress("scherule-announcing")
-                        .setName("scheduling"))
+        super.start()
 
-        val scheduleBusName = config().getString("bus.schedule", "schedule")
-        vertx.eventBus().consumer<JsonObject>(scheduleBusName)
-                .handler { message ->
-                    val quote = message.body()
-                    System.out.println("Schedule!");
-                }
+        messageConsumer = rxVertx.eventBus().consumer("scheduling")
 
-//        val record = EventBusService.createRecord("bus.schedule", scheduleBusName, contentClass)
+        messageConsumer.toObservable().subscribe {
+            log.info("Hello " + it.body())
+        }
 
-        vertx.createHttpServer()
-                .requestHandler({
-                    it.response().end("Hello Vert.x!")
-                })
-        .listen(config().getInteger("http.port"), {
-            if (it.succeeded()) {
-                System.out.println("Server started");
-            } else {
-                System.out.println("Cannot start the server: " + it.cause());
-            }
-        })
-
-        discovery.close()
     }
 
 }
