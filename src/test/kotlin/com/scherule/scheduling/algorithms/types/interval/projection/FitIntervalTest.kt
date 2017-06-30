e6cd15439f721450b4f42ca1cf5bd1463437a045
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-06-30 22:47:25 +0200

    Algorithm skeleton

--- src/test/kotlin/com/scherule/scheduling/algorithms/types/interval/projection/FitIntervalTest.kt
+++ src/test/kotlin/com/scherule/scheduling/algorithms/types/interval/projection/FitIntervalTest.kt
@@ -7,11 +7,29 @@ import org.junit.jupiter.api.Test
 
 internal class FitIntervalTest {
 
+    companion object {
+        val ANY_FITNESS = Fitness(100)
+    }
+
     @Test
-    fun extendingFitIntervalExpandsIntervalAndUsesMinOfAvailabilities() {
+    fun extendingFitIntervalWithInstantBeforeIntervalExpandsFitIntervalStartToContainIt() {
+        val currentFitInterval = FitInterval(Interval.parse("2017-10-03T14:15Z/2017-10-03T16:00Z"), ANY_FITNESS)
+        assertThat(currentFitInterval.expandTo(Instant("2017-10-03T19:00Z"), ANY_FITNESS))
+                .isEqualTo(FitInterval(Interval.parse("2017-10-03T14:15Z/2017-10-03T19:00Z"), ANY_FITNESS))
+    }
+
+    @Test
+    fun extendingFitIntervalWithInstantAfterIntervalExpandsFitIntervalEndToContainIt() {
+        val currentFitInterval = FitInterval(Interval.parse("2017-10-03T14:15Z/2017-10-03T16:00Z"), ANY_FITNESS)
+        assertThat(currentFitInterval.expandTo(Instant("2017-10-03T11:00Z"), ANY_FITNESS))
+                .isEqualTo(FitInterval(Interval.parse("2017-10-03T11:00Z/2017-10-03T16:00Z"), ANY_FITNESS))
+    }
+
+    @Test
+    fun extendingFitIntervalWithSmallerFitnessLowersFitnessToMatchIt() {
         val currentFitInterval = FitInterval(Interval.parse("2017-10-03T14:15Z/2017-10-03T16:00Z"), Fitness(100))
-        assertThat(currentFitInterval.expandTo(Instant("2017-10-03T19:00Z"), Fitness(50)))
-                .isEqualTo(FitInterval(Interval.parse("2017-10-03T14:15Z/2017-10-03T19:00Z"), Fitness(50)))
+        assertThat(currentFitInterval.expandTo(Instant("2017-10-03T15:00Z"), Fitness(50)))
+                .isEqualTo(FitInterval(Interval.parse("2017-10-03T14:15Z/2017-10-03T16:00Z"), Fitness(50)))
     }
 
 }
