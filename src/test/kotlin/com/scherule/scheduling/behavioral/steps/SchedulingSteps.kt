97e6a624674afe19a9418e8eb30970e9527281eb
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-07-04 13:48:24 +0200

    Added feature

--- src/test/kotlin/com/scherule/scheduling/behavioral/steps/SchedulingSteps.kt
+++ src/test/kotlin/com/scherule/scheduling/behavioral/steps/SchedulingSteps.kt
@@ -1,29 +1,43 @@
 package com.scherule.scheduling.behavioral.steps
 
+import com.scherule.scheduling.converters.DurationConverter
+import com.scherule.scheduling.converters.IntervalConverter
+import com.scherule.scheduling.helpers.SchedulingProblemPojo
+import com.scherule.scheduling.helpers.SchedulingProblemPojo.Companion.aSchedulingProblem
+import cucumber.api.Transform
 import cucumber.api.java.en.Given
 import cucumber.api.java.en.When
 import cucumber.runtime.java.guice.ScenarioScoped
+import org.joda.time.Duration
+import org.joda.time.Interval
+import org.joda.time.Period
 
 @ScenarioScoped
-class SchedulingSteps {
+internal class SchedulingSteps {
+
+    var problemBuilder: SchedulingProblemPojo.Builder = aSchedulingProblem()
 
     @Given("there is a meeting scheduling problem")
     fun givenThereIsMeetingSchedulingProblem() {
-        System.out.println("x")
+        problemBuilder = aSchedulingProblem()
     }
 
-    @Given("this meeting has minimum participants count of '(\\d+)'")
-    fun givenThisMeetingHasMinimumParticipantsCountOf() {
-        System.out.println("x")
+    @Given("this meeting has minimum participants count of '(.*)'")
+    fun givenThisMeetingHasMinimumParticipantsCountOf(minParticipants: String) {
+        problemBuilder.withMinParticipants(minParticipants.toInt())
     }
 
-    @Given("this meeting has minimum duration of '(\\d+)' hours")
-    fun givenThisMeetingHasMinimumDurationOfHours() {
-        System.out.println("x")
+    @Given("this meeting has minimum duration of '(.*)' hours")
+    fun givenThisMeetingHasMinimumDurationOfHours(
+            @Transform(DurationConverter::class) minLength: Duration
+    ) {
+        problemBuilder.longerThan(minLength)
     }
 
     @Given("this meeting has to happen in period '(.*)'")
-    fun givenThisMeetingHasToHappenInPeriod() {
+    fun givenThisMeetingHasToHappenInPeriod(
+            @Transform(IntervalConverter::class) interval: Interval
+    ) {
         System.out.println("x")
     }
 
--- src/test/kotlin/com/scherule/scheduling/helpers/SchedulingProblemPojo.kt
+++ src/test/kotlin/com/scherule/scheduling/helpers/SchedulingProblemPojo.kt
@@ -2,6 +2,7 @@ package com.scherule.scheduling.helpers
 
 import com.scherule.scheduling.algorithms.Participation
 import com.scherule.scheduling.algorithms.SchedulingProblem
+import org.joda.time.DateTime
 import org.joda.time.Duration
 import org.joda.time.Interval
 
@@ -26,6 +27,39 @@ internal class SchedulingProblemPojo(
 
     override fun getBetween(): Interval {
         return between
+    }
+
+    companion object {
+        fun aSchedulingProblem() = Builder()
+    }
+
+    private constructor(builder: Builder) : this(
+            minParticipants = builder.minParticipants,
+            minDuration = builder.minDuration,
+            between = builder.between,
+            participation = builder.participation
+    )
+
+    class Builder {
+        var minParticipants = 1
+            private set
+        var minDuration = Duration.ZERO
+            private set
+        var between = Interval.parse("2017-10-02T14:15Z/2017-10-05T16:00Z")
+            private set
+        var participation: MutableSet<Participation> = mutableSetOf()
+            private set
+
+        fun withMinParticipants(minParticipants: Int) = apply { this.minParticipants = minParticipants }
+
+        fun longerThan(minDuration: Duration) = apply { this.minDuration = minDuration }
+
+        fun inBetween(between: Interval) = apply { this.between = between }
+
+        fun withParticipation(vararg participation: Participation) = apply { this.participation.addAll(participation) }
+
+        fun build() = SchedulingProblemPojo(this)
+
     }
 
 }
