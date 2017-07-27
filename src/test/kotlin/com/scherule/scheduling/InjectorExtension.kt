eb9d08db1b262cde311c59f84b7cfd07ffce4971
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-07-27 12:22:25 +0200

    Running integration tests

--- src/test/kotlin/com/scherule/scheduling/InjectorExtension.kt
+++ src/test/kotlin/com/scherule/scheduling/InjectorExtension.kt
@@ -3,11 +3,12 @@ package com.scherule.scheduling
 import com.google.inject.Guice
 import com.google.inject.Injector
 import com.google.inject.Module
+import com.scherule.scheduling.modules.MappingModule
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 
 class InjectorExtension(
-        modules: List<Module> = listOf(SchedulingApplicationContext())
+        modules: List<Module> = listOf(MappingModule())
 ) : BeforeTestExecutionCallback {
 
     val injector: Injector = Guice.createInjector(modules)
